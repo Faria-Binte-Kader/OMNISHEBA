d7e932190604b4fa178f32c73570c8c9e9953d7a
Faria-Binte-Kader <fariabintekader67@gmail.com>
2020-12-16 23:13:14 +0600

    doctor,hospital activity added

---
+++ app/src/main/java/com/example/omnisheba/DoctorMainActivity.java
@@ -0,0 +1,49 @@
+package com.example.omnisheba;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoctorMainActivity extends AppCompatActivity {
+
+    public Button doctorprofilebtn, doctorupdatebtn, doctorpatientbtn,doctorlogoutbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_main_activity);
+        getSupportActionBar().setTitle("0!");
+
+       doctorprofilebtn = (Button) findViewById(R.id.doctorprofilebutton);
+        doctorupdatebtn = (Button) findViewById(R.id.doctorupdatebutton);
+        doctorpatientbtn = (Button) findViewById(R.id.doctorappointmentbutton);
+        doctorlogoutbtn = (Button) findViewById(R.id.doctorlogoutbutton);
+
+    }
+
+
+   public void doctorprofilebutton(View view) {
+      // Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+      // startActivity(intent);
+   }
+    public void doctorupdatebutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+       // startActivity(intent);
+    }
+    public void doctorappointmentbutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+        //startActivity(intent);
+    }
+    public void doctorlogoutbutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+        //startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/omnisheba/HospitalMainActivity.java
@@ -0,0 +1,44 @@
+package com.example.omnisheba;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HospitalMainActivity extends AppCompatActivity {
+
+    public Button hospitalprofilebtn, hospitalupdatebtn, addDoctorbtn,hospitallogoutbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hospital_main_activity);
+        getSupportActionBar().setTitle("0!");
+
+        hospitalprofilebtn = (Button) findViewById(R.id.hospitalprofilebutton);
+        hospitalupdatebtn = (Button) findViewById(R.id.hospitalupdatebutton);
+        addDoctorbtn = (Button) findViewById(R.id.addDoctorbutton);
+        hospitallogoutbtn = (Button) findViewById(R.id.hospitalLogoutbutton);
+
+    }
+
+
+    public void hospitalprofilebutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+        //startActivity(intent);
+   }
+
+    public void hospitalupdatebutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+        //startActivity(intent);
+    }
+    public void addDoctorbutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+        //startActivity(intent);
+    }
+    public void hospitalLogoutbutton(View view) {
+        //Intent intent = new Intent(DoctorMainActivity.this,MainActivity.class);
+        //startActivity(intent);
+    }
+
+}
