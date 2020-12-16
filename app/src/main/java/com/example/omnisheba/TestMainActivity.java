844984299dbd3f7b8271cdf16df884c30374097d
Faria-Binte-Kader <fariabintekader67@gmail.com>
2020-12-16 23:34:41 +0600

    doctor,hospital activity added

---
+++ app/src/main/java/com/example/omnisheba/TestMainActivity.java
@@ -0,0 +1,25 @@
+package com.example.omnisheba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TestMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_main);
+        getSupportActionBar().setTitle("0!");
+    }
+
+    public void testprofilebutton(View view) {
+    }
+
+    public void testupdatebutton(View view) {
+    }
+
+    public void testLogoutbutton(View view) {
+    }
+}
