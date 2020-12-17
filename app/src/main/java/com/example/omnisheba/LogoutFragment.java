1b1defdaeb30dcbf3412ac799653cb0eff3326d9
nhnujhat <nhnujhat@gmail.com>
2020-12-17 16:33:00 +0600

    LogoutFragment,fragment_search,sharedPrefManager updated

--- app/src/main/java/com/example/omnisheba/LogoutFragment.java
+++ app/src/main/java/com/example/omnisheba/LogoutFragment.java
@@ -1,24 +1,85 @@
 package com.example.omnisheba;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class LogoutFragment extends Fragment
+public class LogoutFragment extends Fragment implements View.OnClickListener
 {
+    private Button yesbutton;
+    private Button nobutton;
+
     @Nullable
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {  getActivity().setTitle("0!");
+    {
+        getActivity().setTitle("0!");
         return inflater.inflate(R.layout.fragment_logout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view,savedInstanceState);
+
+        view.findViewById(R.id.yes_btn).setOnClickListener(this);
+        view.findViewById(R.id.no_btn).setOnClickListener(this);
+    }
+
+    private void logout()
+    {
+        SharedPrefManager.getInstance(getActivity()).clear();
+        Intent intent = new Intent(getActivity(),login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void search()
+    {
+        SharedPrefManager.getInstance(getActivity()).clear();
+        Intent intent = new Intent(getActivity(), SearchFragment.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Fragment fragment=null;
+        switch (v.getId())
+        {
+            case R.id.yes_btn:
+                logout();
+                break;
+            case R.id.no_btn:
+                fragment = new SearchFragment();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment someFragment)
+    {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
