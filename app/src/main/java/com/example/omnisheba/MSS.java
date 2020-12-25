1a50592f82adace1209118c6d701f53455ed1c56
Tasmia <tasmia719@gmail.com>
2020-12-25 23:35:49 +0600

    Added CalAge in MSS

--- app/src/main/java/com/example/omnisheba/MSS.java
+++ app/src/main/java/com/example/omnisheba/MSS.java
@@ -1,5 +1,12 @@
 package com.example.omnisheba;
 
+import android.annotation.SuppressLint;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MSS extends Person {
     private String dateofbirth, phone, gender;
 
@@ -34,5 +41,29 @@ public class MSS extends Person {
 
     public String getGender() {
         return gender;
+    }
+
+    public int calcAge() throws ParseException {
+        @SuppressLint("SimpleDateFormat") Date dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(dateofbirth);
+        Calendar today = Calendar.getInstance();
+        Calendar birthDate = Calendar.getInstance();
+        int age = 0;
+        assert dateOfBirth != null;
+        birthDate.setTime(dateOfBirth);
+        if (birthDate.after(today)) {
+            throw new IllegalArgumentException("Can't be born in the future");
+        }
+        age = today.get(Calendar.YEAR) - birthDate.get(Calendar.YEAR);
+        // If birth date is greater than todays date (after 2 days adjustment of leap year) then decrement age one year
+        if ((birthDate.get(Calendar.DAY_OF_YEAR) - today.get(Calendar.DAY_OF_YEAR) > 3) ||
+                (birthDate.get(Calendar.MONTH) > today.get(Calendar.MONTH))) {
+            age--;
+            // If birth date and todays date are of same month and birth day of month is greater than todays day of month then decrement age
+        } else if ((birthDate.get(Calendar.MONTH) == today.get(Calendar.MONTH)) &&
+                (birthDate.get(Calendar.DAY_OF_MONTH) > today.get(Calendar.DAY_OF_MONTH))) {
+            age--;
+        }
+
+        return age;
     }
 }
